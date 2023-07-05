8fc69f8661a9623257c5b3c2f62ee97538524090
AKHNAFAL <126429884+AKHNAFAL@users.noreply.github.com>
2023-07-05 14:20:01 +0700

    push

--- src/View/LaporanKegiatanCTRL.java
+++ src/View/LaporanKegiatanCTRL.java
@@ -1,5 +1,17 @@
 package View;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class LaporanKegiatanCTRL {
-    
+
+    @FXML
+    private VBox VBoxJadwal;
+
+    @FXML
+    void addArtikel(ActionEvent event) {
+
+    }
+
 }
