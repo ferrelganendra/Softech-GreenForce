30a4c06994d013a3794b0207dfa128e3579d75f1
Ferrel Ganendra <ferrelganendra25@gmail.com>
2023-07-09 14:20:45 +0700

    commit lagi

--- src/LaporanKegiatan/LaporanCardCTRL.java
+++ src/LaporanKegiatan/LaporanCardCTRL.java
@@ -1,5 +1,22 @@
 package LaporanKegiatan;
 
+import Main.MainPaneCTRL;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import util.OpenScene;
+
 public class LaporanCardCTRL {
-    
+
+    @FXML
+    private Button button;
+
+    @FXML
+    void keLaporanKegiatan(ActionEvent event) {
+        OpenScene object = new OpenScene();
+        Pane halaman = object.getPane("/LaporanKegiatan/LaporanKegiatanEdit");
+        MainPaneCTRL.getInstance().getMainPane().setCenter(halaman);
+    }
+
 }
